a250afff79d2d6ead545a7e2e9961ef488a9afe0
Khai Nguyen <khainguyen@bbv.ch>
2018-05-11 13:19:56 +0700

    Use publishing event instead of request

--- source/Microservice.Member.Infrastructure/MemberContext.cs
+++ source/Microservice.Member.Infrastructure/MemberContext.cs
@@ -1,24 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using MassTransit;
 using Microservice.Core.DataAccess.MongoDB;
 using Microservice.Core.Interfaces;
 using Microservice.Core.Models;
-using Microsoft.Extensions.Configuration;
 
 namespace Microservice.Member.Infrastructure
 {
     public class MemberContext : MongoDbContext
     {
         private readonly IBusControl _busControl;
-        private readonly IConfiguration _configuration;
 
         public List<IEvent> Events { get; private set; }
 
-        public MemberContext(string connectionString, string databaseName, bool isSsl, IBusControl busControl, IConfiguration configuration) : base(connectionString, databaseName, isSsl)
+        public MemberContext(string connectionString, string databaseName, bool isSsl, IBusControl busControl) : base(connectionString, databaseName, isSsl)
         {
             _busControl = busControl;
-            _configuration = configuration;
         }
 
         public void AddEvents(DomainEntity entity)
@@ -39,16 +35,9 @@ namespace Microservice.Member.Infrastructure
                     ModelName = @event.GetType().Name,
                     Data = @event.Data
                 };
-                var serviceAddress = GetServiceAddress("member_service");
-                var client = _busControl.CreateRequestClient<Event, Object>(serviceAddress, TimeSpan.FromSeconds(500));
-                client.Request(eventData);
-            }
-        }
 
-        private Uri GetServiceAddress(string address)
-        {
-            var serviceAddress = new Uri($"{_configuration.GetConnectionString("RabbitMQHost")}/{address}");
-            return serviceAddress;
+                _busControl.Publish(eventData);
+            }
         }
     }
 }
