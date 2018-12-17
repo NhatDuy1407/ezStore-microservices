93acc95cc60fc3ff4d18126317387e2f711edc67
Khai Nguyen <khainguyen@bbv.ch>
2018-12-17 11:52:50 +0700

    Update ...

---
+++ source/03.ezStore/Product/ezStore.Product.ApplicationCore/Mapper/ManufactureMapper.cs
@@ -0,0 +1,41 @@
+﻿using ezStore.Product.ApplicationCore.Dtos;
+using ezStore.Product.ApplicationCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ezStore.Product.ApplicationCore.Mapper
+{
+    public static class ManufactureMapper
+    {
+        public static Manufacture DtoToEntity(ManufactureDto dto)
+        {
+            return new Manufacture
+            {
+                Id = dto.Id,
+                Name = dto.Name,
+                CreatedBy = dto.CreatedBy,
+                UpdatedBy = dto.UpdatedBy,
+                UpdatedDate = DateTime.Now
+            };
+        }
+
+        public static ManufactureDto EntityToDto(Manufacture dto)
+        {
+            return new ManufactureDto
+            {
+                Id = dto.Id,
+                Name = dto.Name,
+                CreatedBy = dto.CreatedBy,
+                UpdatedBy = dto.UpdatedBy,
+                CreatedDate = dto.CreatedDate,
+                UpdatedDate = dto.UpdatedDate,
+            };
+        }
+
+        public static IEnumerable<ManufactureDto> EntityToDtos(IEnumerable<Manufacture> entities)
+        {
+            return entities?.Select(EntityToDto);
+        }
+    }
+}
---
+++ source/03.ezStore/Product/ezStore.Product.ApplicationCore/ProductAggregate/ManufactureDomain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ezStore.Product.ApplicationCore.Dtos;
+using ezStore.Product.ApplicationCore.Entities;
+using ezStore.Product.ApplicationCore.Mapper;
+using Ws4vn.Microservicess.ApplicationCore.Entities;
+using Ws4vn.Microservicess.ApplicationCore.Interfaces;
+
+namespace ezStore.Product.ApplicationCore.ProductAggregate
+{
+    public class ManufactureDomain : AggregateRoot
+    {
+        public ManufactureDomain(IDataAccessService dataAccessService) : base(dataAccessService)
+        {
+        }
+
+        internal void Add(ManufactureDto manufatureDto)
+        {
+            var newCategory = ManufactureMapper.DtoToEntity(manufatureDto);
+            dataAccessService.Repository<Manufacture>().Insert(newCategory);
+        }
+    }
+}
