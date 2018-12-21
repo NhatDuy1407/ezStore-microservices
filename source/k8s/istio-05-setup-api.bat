92b185f06ac86851da2a586d3c4d09312c2def18
Khai Nguyen <khainguyen@bbv.ch>
2018-12-21 11:54:52 +0700

    Update Readme

--- source/k8s/istio-05-setup-api.bat
+++ source/k8s/istio-05-setup-api.bat
@@ -1,5 +1,2 @@
 istioctl kube-inject -f k8s\k8s.service.api.yaml -o k8s\k8s.service.api.istio.yaml
 kubectl apply -f k8s\k8s.service.api.istio.yaml
-
-kubectl -n istio-system port-forward istio-egressgateway-56bdd5fcfb-rnwbc 30101
-REM kubectl -n istio-system port-forward istio-egressgateway-56bdd5fcfb-rnwbc 30102
